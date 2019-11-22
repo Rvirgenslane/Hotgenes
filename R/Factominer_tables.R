92c51ed01e369c91299c78cf3ff3b76acb6e6898
Richard Virgen-Slane <richardvirgen-slane@ware3736.burnham.org>
2019-11-21 16:25:44 -0800

    Table formatting added

--- R/Factominer_tables.R
+++ R/Factominer_tables.R
@@ -4,6 +4,8 @@
 #' from FactoMineR
 #' @param Round_To numeric value indicating digit for rounding
 #' default = 2
+#' @param Format Logical, if TRUE (default), output formated by
+#' group.
 #' @return data.frame with HCPC results
 #' @examples Example_Hotgenes_dir<-system.file("extdata",
 #' "Example_Hotgenes.Rdata",
@@ -13,7 +15,36 @@
 #' Quanti_Table(Results$res.hcpc)
 
 # FactoMiner Functions ----------------------------------------------------
-Quanti_Table<-function(res.hcpc=NULL, Round_To = 2){
+Quanti_Table<-function(res.hcpc=NULL, Round_To = 2,
+                       Format = TRUE){
+    
+# Table_Express 
+    Table_Express<-function(df_cat){
+        EqDatedf_cat <- as.data.frame(df_cat[1,])
+        
+        cat_cols<-colnames(df_cat)
+        df_cat_lists <- setNames(vector(length(cat_cols), mode="list"), cat_cols)
+        
+        for (i in cat_cols){
+            df_cat_lists[[i]]<-("")
+        }
+        EmptyLine <- data.frame(df_cat_lists, check.names = FALSE)
+        
+        
+        for (i in 2:nrow(df_cat)){
+            if(as.vector(df_cat$Groups[i])  ==  as.vector(df_cat$Groups[i-1])){
+                EqDatedf_cat <- rbind(EqDatedf_cat, df_cat[i,])
+            } else {
+                EqDatedf_cat <- rbind(EqDatedf_cat, EmptyLine)
+                EqDatedf_cat <- rbind(EqDatedf_cat, df_cat[i,])
+            }
+        }
+        
+        rownames(EqDatedf_cat)<-NULL
+        return(EqDatedf_cat)
+    }
+    
+    
     
     # Variable
     df_quanti <- plyr::ldply(res.hcpc$desc.var$quanti, data.frame)
@@ -30,7 +61,12 @@ Quanti_Table<-function(res.hcpc=NULL, Round_To = 2){
     colnames(df_quanti)<-gsub(".", " ",colnames(df_quanti), fixed = TRUE)
     colnames(df_quanti)<-gsub("Variable", "Genes",colnames(df_quanti), fixed = TRUE)
     
+    if(Format == TRUE){
+        Table_df_quanti<-Table_Express(df_quanti)
+        return(Table_df_quanti)
+    }else if(Format != TRUE){
     return(df_quanti)
+    }    
 }
 
 
@@ -41,6 +77,8 @@ Quanti_Table<-function(res.hcpc=NULL, Round_To = 2){
 #' from FactoMineR
 #' @param Round_To numeric value indicating digit for rounding
 #' default = 2
+#' @param Format Logical, if TRUE (default), output formated by
+#' group.
 #' @return data.frame with HCPC results
 #' @examples Example_Hotgenes_dir<-system.file("extdata",
 #' "Example_Hotgenes.Rdata",
@@ -50,7 +88,35 @@ Quanti_Table<-function(res.hcpc=NULL, Round_To = 2){
 #' Categorical_Table(Results$res.hcpc)
 
 # Categorical table ------------------------------------------------------------
-Categorical_Table<-function(res.hcpc = NULL, Round_To = 2){
+Categorical_Table<-function(res.hcpc = NULL, Round_To = 2,
+                            Format=TRUE){
+    
+    # Table_Express 
+    Table_Express<-function(df_cat){
+        EqDatedf_cat <- as.data.frame(df_cat[1,])
+        
+        cat_cols<-colnames(df_cat)
+        df_cat_lists <- setNames(vector(length(cat_cols), mode="list"), cat_cols)
+        
+        for (i in cat_cols){
+            df_cat_lists[[i]]<-("")
+        }
+        EmptyLine <- data.frame(df_cat_lists, check.names = FALSE)
+        
+        
+        for (i in 2:nrow(df_cat)){
+            if(as.vector(df_cat$Groups[i])  ==  as.vector(df_cat$Groups[i-1])){
+                EqDatedf_cat <- rbind(EqDatedf_cat, df_cat[i,])
+            } else {
+                EqDatedf_cat <- rbind(EqDatedf_cat, EmptyLine)
+                EqDatedf_cat <- rbind(EqDatedf_cat, df_cat[i,])
+            }
+        }
+        
+        rownames(EqDatedf_cat)<-NULL
+        return(EqDatedf_cat)
+    }
+    
     
     # Categories
     df_cat <- plyr::ldply(res.hcpc$desc.var$category, data.frame)
@@ -76,6 +142,12 @@ Categorical_Table<-function(res.hcpc = NULL, Round_To = 2){
         df_cat[,c(i)]<-paste(df_cat[,c(i)], "%", sep = "")
     }
     
-    return(df_cat)
+    if(Format == TRUE){
+        Table_df_cat<-Table_Express(df_cat)
+        return(Table_df_cat)
+    }else if(Format != TRUE){
+        return(df_cat)
+    }   
+    
 }
 
